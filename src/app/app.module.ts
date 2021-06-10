feed491e1b28637d7715e17cf94d53bb8896f539
Cornelius Okeke <okekecornelius@gmail.com>
2021-06-10 14:42:25 +0100

    first init ugwueme website

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,15 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderModule } from './components/header/header.module';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+
+    HeaderModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/services/utility.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+import { Title, Meta, MetaDefinition } from '@angular/platform-browser';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UtilityService {
+
+  constructor(
+    private title: Title,
+    private meta: Meta,
+  ) {
+  }
+
+
+  setPageTitle(pageTitle: string, withBranchName = false) {
+    this.title.setTitle(`${pageTitle} - Ugwueme Autonomous Community`);
+  }
+
+  setPageMetaData(metaData: MetaDefinition[]) {
+    this.meta.addTags(metaData);
+  }
+
+}
